0ff61004d1515cd8265219c059fee857c1ca0fa8
Daniel Zduniak <dzduniak@gmail.com>
2016-07-27 14:42:02 +0200

    Cleanup

--- out/production/worker/worker.js
+++
@@ -1,9 +0,0 @@
-(function (Kotlin) {
-  'use strict';
-  var _ = Kotlin.defineRootPackage(null, /** @lends _ */ {
-    test: function () {
-      Kotlin.println('Sialalalala');
-    }
-  });
-  Kotlin.defineModule('worker', _);
-}(Kotlin));
